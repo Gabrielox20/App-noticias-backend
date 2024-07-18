4b2a379def815b0021ea099ee7d52c024dbe586e
207814636 <gabriel.gonzalez1@mail.udp.cl>
2024-07-18 03:47:28 -0400

    prueba

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { execSync } = require('child_process');
 
 dotenv.config();
 

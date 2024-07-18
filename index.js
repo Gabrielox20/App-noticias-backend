3a934f892da29fefe611392595ddd75b21255f04
207814636 <gabriel.gonzalez1@mail.udp.cl>
2024-07-18 04:00:20 -0400

    prueba

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-// Instalar dependencias de Python
-try {
-  execSync('pip install -r requirements.txt');
-  console.log('Dependencias de Python instaladas');
-} catch (error) {
-  console.error('Error instalando dependencias de Python:', error.message);
-}
 
 // Middleware
 app.use(express.json());
@@ -42,14 +35,30 @@ app.get('/', (req, res) => {
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err.message);
-  });
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
 
 // Integrar syncLeagues.js y syncNews.js
+// Verificar instalación de Python y pip
+try {
+  const pythonVersion = execSync('python --version');
+  const pipVersion = execSync('pip --version');
+  console.log('Python version:', pythonVersion.toString());
+  console.log('Pip version:', pipVersion.toString());
+} catch (error) {
+  console.error('Error verificando la instalación de Python y pip:', error.message);
+}
+// Instalar dependencias de Python
+try {
+  execSync('pip install -r requirements.txt');
+  console.log('Dependencias de Python instaladas');
+} catch (error) {
+  console.error('Error instalando dependencias de Python:', error.message);
+}
 require('./syncLeagues');
 require('./syncNews');
 
